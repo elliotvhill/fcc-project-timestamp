52ea6fc31d9ee23dad50ba74a513e557492e90b8
Elliot Hill (he/him) <elliotvh@icloud.com>
2024-12-02 12:45:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date", (req, res) => {
+    // Get params from URL
+    const date = req.params.date;
+    // Check if date passed matches YYYY-MM-DD format
+    const dateRegex = /^[\d]{4}-[\d]{2}-[\d]{2}$/i;
+    const unixRegex = /^[\d]+$/i;
+    console.log("dateRegex: ", dateRegex.test(date), "unixRegex: ", unixRegex.test(date))
+        // If date is in YYYY-MM-DD format
+        if (dateRegex.test(date)) {
+            console.log("if block"); // TODO: remove
+            // Parse date to unix timestamp and convert date to utc string
+            return res.json({ unix: Date.parse(date), utc: new Date(date).toUTCString() });
+        } else if (unixRegex.test(date)) {
+            console.log("else block"); // TODO: remove
+            // Return date as Int and convert to utc date string
+            return res.json({ unix: parseInt(date), utc: new Date(parseInt(date)).toUTCString() })
+        } else {
+        res.json({ error: "Invalid Date" });
+    };
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
